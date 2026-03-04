26d8591a6cf638d2068e5dfa54fdd15572a3167b
devlikepro <waha@devlike.pro>
2026-03-04 18:21:18 +0700

    [core] media - fetch user-agent

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,15 +1,20 @@
 import axios from 'axios';
 import { Agent as HttpsAgent } from 'https';
+import UserAgent from 'user-agents';
 
 const InsecureHttpsAgent = new HttpsAgent({
   rejectUnauthorized: false,
 });
 
 export async function fetchBuffer(url: string): Promise<Buffer> {
+  const userAgent = new UserAgent();
   return axios
     .get(url, {
       responseType: 'arraybuffer',
       httpsAgent: InsecureHttpsAgent,
+      headers: {
+        'User-Agent': userAgent.toString(),
+      },
     })
     .then((res) => {
       return Buffer.from(res.data);
